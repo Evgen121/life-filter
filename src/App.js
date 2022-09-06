39c4e721a23e19134cacccb20f597760655892fa
Yevhen Malyi <malyje164@gmail.com>
2022-09-06 19:42:06 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
 function App() {
+const [loading, setLoging]=useState(false)
+const [posts,setPosts] =useState([])
+const [searchTitle, setSearchTitle]=useState('')
+
+const loadingPost=async()=>{
+  setLoging(true)
+  const res= await axios.get('https://jsonplaceholder.typicode.com/posts')
+  setPosts(res.data)
+  setLoging(false)
+}
+
+useEffect(()=>{
+ loadingPost()
+},[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h4>Search filter</h4>
+      <input 
+      style={{width:"30%",height:"35px"}}
+      type="text"
+      placeholder='Searc ...'
+      onChange={(e)=> setSearchTitle(e.target.value)}/>
+      {loading ? (<h4>Loading ...</h4>):(
+        (posts.filter((value)=>{
+          if(searchTitle ===""){
+            return value
+          }else if(
+            value.title.toLowerCase().includes(searchTitle.toLocaleLowerCase())
+          ){
+            return value
+          }
+        }).map(post=>
+        <h5 key={post.id}>{post.title}</h5>))
+      )}
     </div>
   );
 }
